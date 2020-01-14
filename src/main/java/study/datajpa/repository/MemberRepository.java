c328784796d50cd8daadc7166fbe831ef1e6c23f
kwanghyunMoon <mgh3326@naver.com>
2020-01-14 22:35:42 +0900

    5_쿼리 메소드 기능

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package study.datajpa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,6 +34,44 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   List<Member> findListByUsername(String name); //컬렉션
 
   Member findMemberByUsername(String name); //단건
+
   Optional<Member> findOptionalByUsername(String name); //단건 Optional
+
+  Page<Member> findByAge(int age, Pageable pageable);
+
+  @Query(value = "select m from Member m",
+          countQuery = "select count(m.username) from Member m")
+  Page<Member> findMemberAllCountBy(Pageable pageable);
+
+  @Modifying(clearAutomatically = true)
+  @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+  int bulkAgePlus(@Param("age") int age);
+
+  @Query("select m from Member m left join fetch m.team")
+  List<Member> findMemberFetchJoin();
+
+  //공통 메서드 오버라이드
+  @Override
+  @EntityGraph(attributePaths = {"team"})
+  List<Member> findAll();
+
+  //JPQL + 엔티티 그래프
+  @EntityGraph(attributePaths = {"team"})
+  @Query("select m from Member m")
+  List<Member> findMemberEntityGraph();
+
+  //메서드 이름으로 쿼리에서 특히 편리하다.
+  @EntityGraph(attributePaths = {"team"})
+  List<Member> findByUsername(@Param("username") String username);
+
+  @EntityGraph("Member.all")
+  @Query("select m from Member m")
+  List<Member> findMember2EntityGraph();
+
+  @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+  Member findReadOnlyByUsername(String username);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  List<Member> findLockByUsername(String name);
 }
 
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,10 +3,15 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +22,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
   @Autowired
   MemberRepository memberRepository;
+  @Autowired
+  TeamRepository teamRepository;
+  @Autowired
+  EntityManager em;
 
   @Test
   public void testMember() {
@@ -63,5 +72,87 @@ class MemberRepositoryTest {
     assertThat(result.get(0).getUsername()).isEqualTo("AAA");
     assertThat(result.get(0).getAge()).isEqualTo(20);
     assertThat(result.size()).isEqualTo(1);
+  }
+
+  //페이징 조건과 정렬 조건 설정
+  @Test
+  public void page() throws Exception {
+//given
+    memberRepository.save(new Member("member1", 10));
+    memberRepository.save(new Member("member2", 10));
+    memberRepository.save(new Member("member3", 10));
+    memberRepository.save(new Member("member4", 10));
+    memberRepository.save(new Member("member5", 10));
+//when
+    PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC,
+            "username"));
+    Page<Member> page = memberRepository.findByAge(10, pageRequest);
+//then
+    List<Member> content = page.getContent(); //조회된 데이터
+    assertThat(content.size()).isEqualTo(3); //조회된 데이터 수
+    assertThat(page.getTotalElements()).isEqualTo(5); //전체 데이터 수
+    assertThat(page.getNumber()).isEqualTo(0); //페이지 번호
+    assertThat(page.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+    assertThat(page.isFirst()).isTrue(); //첫번째 항목인가?
+    assertThat(page.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+    Page<Member> page2 = memberRepository.findByAge(10, pageRequest);
+    Page<MemberDto> dtoPage = page2.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+  }
+
+  @Test
+  public void bulkUpdate() throws Exception {
+//given
+    memberRepository.save(new Member("member1", 10));
+    memberRepository.save(new Member("member2", 19));
+    memberRepository.save(new Member("member3", 20));
+    memberRepository.save(new Member("member4", 21));
+    memberRepository.save(new Member("member5", 40));
+//when
+    int resultCount = memberRepository.bulkAgePlus(20);
+//then
+    assertThat(resultCount).isEqualTo(3);
+  }
+
+  @Test
+  public void findMemberLazy() throws Exception {
+//given
+    //member1 -> teamA
+//member2 -> teamB
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    teamRepository.save(teamA);
+    teamRepository.save(teamB);
+    memberRepository.save(new Member("member1", 10, teamA));
+    memberRepository.save(new Member("member2", 20, teamB));
+    em.flush();
+    em.clear();
+//when
+    List<Member> members = memberRepository.findAll();
+//then
+    for (Member member : members) {
+      member.getTeam().getName();
+    }
+  }
+
+  @Test
+  public void queryHint() throws Exception {
+//given
+    memberRepository.save(new Member("member1", 10));
+    em.flush();
+    em.clear();
+//when
+    Member member = memberRepository.findReadOnlyByUsername("member1");
+    member.setUsername("member2");
+    em.flush(); //Update Query 실행X
+  }
+  @Test
+  public void lock(){
+    //given
+    memberRepository.save(new Member("member1", 10));
+    em.flush();
+    em.clear();
+//when
+    Member member = memberRepository.findReadOnlyByUsername("member1");
   }
 }
